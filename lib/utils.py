943fcf51137e8e819e38f10330b1825e7ac71f22
Mikhail Kocherov <ppchaos@mail.ru>
2026-01-15 23:41:00 +0300

    s3_stacked_dae_07.ipynb. Developing further

--- lib/utils.py
+++ lib/utils.py
@@ -77,20 +77,66 @@ class MathUtils:
         return (andiff + 180) % 360 - 180
 
 ### 
-# from https://gist.github.com/parente/691d150c934b89ce744b5d54103d7f1e
-def _html_src_from_raw_image_data(data):
-    """Base64 encodes image bytes for inclusion in an HTML img element"""
-    img_obj = IPython.display.Image(data=data)
-    for bundle in img_obj._repr_mimebundle_():
-        for mimetype, b64value in bundle.items():
-            if mimetype.startswith('image/'):
-                return f'data:{mimetype};base64,{b64value}'
+class PILUtils:
+    @staticmethod
+    # from https://gist.github.com/parente/691d150c934b89ce744b5d54103d7f1e
+    def _html_src_from_raw_image_data(data):
+        """Base64 encodes image bytes for inclusion in an HTML img element"""
+        img_obj = IPython.display.Image(data=data)
+        for bundle in img_obj._repr_mimebundle_():
+            for mimetype, b64value in bundle.items():
+                if mimetype.startswith('image/'):
+                    return f'data:{mimetype};base64,{b64value}'
 
-def display_images(images, captions=None, row_height='auto'):
-    figures = []
+    @staticmethod
+    def display_images(images, captions=None, row_height='auto'):
+        figures = []
+        
+        for image_index, image in enumerate(images):
+            if isinstance(image, bytes) or isinstance(image, Image.Image):
+                if isinstance(image, bytes):
+                    bts = image
+                else:
+                    b = io.BytesIO()
+                    image.save(b, format='PNG')
+                    bts = b.getvalue()
+                
+                src = _html_src_from_raw_image_data(bts)
+            else:
+                src = image
+                #caption = f'<figcaption style="font-size: 0.6em">{image}</figcaption>'
     
-    for image_index, image in enumerate(images):
-        if isinstance(image, bytes) or isinstance(image, Image.Image):
+            caption = ''
+            
+            if not captions is None:
+                if isinstance(captions, dict):
+                    caption = captions.get(id(image), '')
+                else:
+                    assert len(captions) == len(images)
+                    caption = captions[image_index]
+    
+                if caption:
+                    caption = f'<figcaption style="font-size: 0.6em">{caption}</figcaption>'
+            
+            figures.append(f'''
+                <figure style="margin: 5px !important;">
+                  <img src="{src}" style="height: {row_height}">
+                  {caption}
+                </figure>
+            ''')
+        return IPython.display.HTML(data=f'''
+            <div style="display: flex; flex-flow: row wrap; text-align: center;">
+            {''.join(figures)}
+            </div>
+        ''')
+
+    @staticmethod
+    def display_images_grid(images, col_count, col_width=None, captions=None):
+        figures = []
+        
+        for image_index, image in enumerate(images):
+            assert isinstance(image, bytes) or isinstance(image, Image.Image)
+    
             if isinstance(image, bytes):
                 bts = image
             else:
@@ -99,101 +145,73 @@ def display_images(images, captions=None, row_height='auto'):
                 bts = b.getvalue()
             
             src = _html_src_from_raw_image_data(bts)
-        else:
-            src = image
-            #caption = f'<figcaption style="font-size: 0.6em">{image}</figcaption>'
-
-        caption = ''
-        
-        if not captions is None:
-            if isinstance(captions, dict):
-                caption = captions.get(id(image), '')
-            else:
-                assert len(captions) == len(images)
-                caption = captions[image_index]
-
-            if caption:
-                caption = f'<figcaption style="font-size: 0.6em">{caption}</figcaption>'
-        
-        figures.append(f'''
-            <figure style="margin: 5px !important;">
-              <img src="{src}" style="height: {row_height}">
-              {caption}
-            </figure>
-        ''')
-    return IPython.display.HTML(data=f'''
-        <div style="display: flex; flex-flow: row wrap; text-align: center;">
-        {''.join(figures)}
-        </div>
-    ''')
-
-def display_images_grid(images, col_count, col_width=None, captions=None):
-    figures = []
     
-    for image_index, image in enumerate(images):
-        assert isinstance(image, bytes) or isinstance(image, Image.Image)
-
-        if isinstance(image, bytes):
-            bts = image
+            caption = ''
+    
+            if not captions is None:
+                if isinstance(captions, dict):
+                    caption = str(captions.get(id(image), ''))
+                else:
+                    assert len(captions) == len(images), (len(captions), len(images))
+                    caption = str(captions[image_index])
+    
+                if caption:
+                    caption = f'<figcaption style="font-size: 0.6em">{caption}</figcaption>'
+            
+            figures.append(f'''
+                <figure style="margin: 5px !important;">
+                  <img src="{src}" style="height: auto">
+                  {caption}
+                </figure>
+            ''')
+    
+        if not col_width:
+            if len(images) > 0 and isinstance(images[0], Image.Image):
+                col_width = images[0].width
+    
+        if not col_width: 
+            col_width='auto'
         else:
-            b = io.BytesIO()
-            image.save(b, format='PNG')
-            bts = b.getvalue()
-        
-        src = _html_src_from_raw_image_data(bts)
+            col_width = f'{col_width}px'
+            
+        return IPython.display.HTML(data=f'''<div style="
+            display: grid; 
+            grid-template-columns: repeat({col_count}, {col_width});
+            column-gap: 1px;
+            row-gap: 1px;">
+            {''.join(figures)}
+        </div>''')
 
-        caption = ''
+    @staticmethod
+    def matrix_to_image(m):
+        m = m.ravel()
+        sz = int(np.sqrt(m.shape[0]))
+        assert sz * sz == m.shape[0]
+        return Image.frombytes('L', size=(sz, sz), data=m.astype('b'))
 
-        if not captions is None:
-            if isinstance(captions, dict):
-                caption = str(captions.get(id(image), ''))
-            else:
-                assert len(captions) == len(images), (len(captions), len(images))
-                caption = str(captions[image_index])
+    @staticmethod
+    def lay_grid(image, step=16):
+        draw = ImageDraw.Draw(image)
+    
+        for c in range(step - 1, image.height, step):
+            draw.line([0, c, image.width, c], fill='gray')
+            draw.line([c, 0, c, image.height], fill='gray')
+    
+        return image
 
-            if caption:
-                caption = f'<figcaption style="font-size: 0.6em">{caption}</figcaption>'
-        
-        figures.append(f'''
-            <figure style="margin: 5px !important;">
-              <img src="{src}" style="height: auto">
-              {caption}
-            </figure>
-        ''')
+###
+class ArrayUtils:
+    @staticmethod
+    def v2sm(v, pad_value=None):
+        assert v.ndim == 1
+        root = np.sqrt(v.shape[0])
+        sz = int(root)
 
-    if not col_width:
-        if len(images) > 0 and isinstance(images[0], Image.Image):
-            col_width = images[0].width
-
-    if not col_width: 
-        col_width='auto'
-    else:
-        col_width = f'{col_width}px'
-        
-    return IPython.display.HTML(data=f'''<div style="
-        display: grid; 
-        grid-template-columns: repeat({col_count}, {col_width});
-        column-gap: 1px;
-        row-gap: 1px;">
-        {''.join(figures)}
-    </div>''')
-
-def vec_to_square_matrix(v):
-    sz = int(np.sqrt(v.shape[0]))
-    assert sz * sz == v.shape[0]
-    return v.reshape(sz, -1)
-
-def matrix_to_image(m):
-    m = m.ravel()
-    sz = int(np.sqrt(m.shape[0]))
-    assert sz * sz == m.shape[0]
-    return Image.frombytes('L', size=(sz, sz), data=m.astype('b'))
-
-def lay_grid(image, step=16):
-    draw = ImageDraw.Draw(image)
-
-    for c in range(step - 1, image.height, step):
-        draw.line([0, c, image.width, c], fill='gray')
-        draw.line([c, 0, c, image.height], fill='gray')
-
-    return image
+        if sz * sz != v.shape[0]:
+            assert pad_value is not None, f'Failed to make square matrix without padding from vector of size {v.shape}'
+            sz = int(np.ceil(root))
+            pad_elems_count = (sz * sz) - len(v)
+            assert pad_elems_count > 0
+            return np.r_[v, np.full(pad_elems_count, pad_value)].reshape(sz, sz)
+        else:
+            return v.reshape(sz, sz)
