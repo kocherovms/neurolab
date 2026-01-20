e220ddbc97818822591b6c948496b50b9d6ba194
Mikhail Kocherov <ppchaos@mail.ru>
2026-01-20 15:54:23 +0300

    Reconnect for RmqSummaryWriter + more logging for collector

--- lib/metrics_collector.py
+++ lib/metrics_collector.py
@@ -6,9 +6,12 @@ import json
 from functools import lru_cache
 
 import pika
+import pika.exceptions
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.backends.backend_agg as plt_backend_agg
+
+import torch
 
 RMQ_EVENTS_EXCHANGE_NAME = 'events'
 RMQ_EVENTS_QUEUE_NAME = 'events'
@@ -16,10 +19,9 @@ RMQ_DEFAULT_CONNECTION_URL = 'amqp://guest:guest@rabbitmq:5672/%2F'
 
 class RmqSummaryBase:
     def __init__(self, rmq_connection_url=RMQ_DEFAULT_CONNECTION_URL):
-        connection_parameters = pika.URLParameters(rmq_connection_url)
-        self.connection = pika.BlockingConnection(connection_parameters)
-        self.channel = self.connection.channel()
-        self.exchange = self.channel.exchange_declare(
+        self.connection_parameters = pika.URLParameters(rmq_connection_url)
+        self.reconnect()
+        exchange = self.channel.exchange_declare(
             exchange=RMQ_EVENTS_EXCHANGE_NAME, 
             exchange_type='topic',
             durable=True,
@@ -34,6 +36,10 @@ class RmqSummaryBase:
             queue=RMQ_EVENTS_QUEUE_NAME,
         )
 
+    def reconnect(self):
+        self.connection = pika.BlockingConnection(self.connection_parameters)
+        self.channel = self.connection.channel()
+        
 class RmqSummaryWriter(RmqSummaryBase):
     def __init__(self, log_dir, rmq_connection_url=RMQ_DEFAULT_CONNECTION_URL):
         super().__init__(rmq_connection_url)
@@ -45,23 +51,17 @@ class RmqSummaryWriter(RmqSummaryBase):
         properties.headers['global_step'] = str(global_step)
         
         with io.BytesIO() as b:
+            if isinstance(scalar_value, torch.Tensor):
+                scalar_value = scalar_value.item()
+                
             pickle.dump(scalar_value, b)
-            self.channel.basic_publish(
-                exchange=RMQ_EVENTS_EXCHANGE_NAME, 
-                routing_key=RMQ_EVENTS_QUEUE_NAME, 
-                body=b.getvalue(),
-                properties=properties)
+            self._robust_publish(body=b.getvalue(), properties=properties)
 
     def add_text(self, tag, text_string, global_step):
         properties = self._create_message_properties('add_text')
         properties.headers['tag'] = tag
         properties.headers['global_step'] = str(global_step)
-        
-        self.channel.basic_publish(
-            exchange=RMQ_EVENTS_EXCHANGE_NAME, 
-            routing_key=RMQ_EVENTS_QUEUE_NAME, 
-            body=text_string.encode(),
-            properties=properties)
+        self._robust_publish(body=text_string.encode(), properties=properties)
         
     def add_figure(self, tag, figure, global_step, close):
         properties = self._create_message_properties('add_figure')
@@ -71,11 +71,7 @@ class RmqSummaryWriter(RmqSummaryBase):
         with io.BytesIO() as b:
             image = self._figure_to_image(figure, close)
             pickle.dump(image, b)
-            self.channel.basic_publish(
-                exchange=RMQ_EVENTS_EXCHANGE_NAME, 
-                routing_key=RMQ_EVENTS_QUEUE_NAME, 
-                body=b.getvalue(),
-                properties=properties)
+            self._robust_publish(body=b.getvalue(), properties=properties)
 
     def add_hparams(self, hparam_dict, metric_dict, run_name):
         properties = self._create_message_properties('add_hparams')
@@ -83,21 +79,26 @@ class RmqSummaryWriter(RmqSummaryBase):
 
         message = dict(hparam_dict=hparam_dict, metric_dict=metric_dict)
         body = json.dumps(message)
-        
-        self.channel.basic_publish(
-            exchange=RMQ_EVENTS_EXCHANGE_NAME, 
-            routing_key=RMQ_EVENTS_QUEUE_NAME, 
-            body=body.encode(),
-            properties=properties)
+        self._robust_publish(body=body.encode(), properties=properties)
 
     def flush(self):
         properties = self._create_message_properties('flush')
+        self._robust_publish(body='', properties=properties)
 
-        self.channel.basic_publish(
-            exchange=RMQ_EVENTS_EXCHANGE_NAME, 
-            routing_key=RMQ_EVENTS_QUEUE_NAME, 
-            body='',
-            properties=properties)
+    def _robust_publish(self, body, properties):
+        for attempt_no in range(2):
+            try:
+                self.channel.basic_publish(
+                    exchange=RMQ_EVENTS_EXCHANGE_NAME, 
+                    routing_key=RMQ_EVENTS_QUEUE_NAME, 
+                    body=body,
+                    properties=properties)
+                break
+            except (pika.exceptions.StreamLostError, pika.exceptions.ConnectionClosedByBroker) as e:
+                if attempt_no == 0:
+                    self.reconnect()
+                else:
+                    raise
 
     def _figure_to_image(self, figure, close):
         canvas = plt_backend_agg.FigureCanvasAgg(figure)
@@ -147,28 +148,28 @@ class RmqSummaryCollector(RmqSummaryBase):
                 with io.BytesIO(body) as b:
                     scalar_value = pickle.load(b)
                     self.get_summary_writer(log_dir).add_scalar(tag, scalar_value, global_step)
-                    print(f'add_scalar, tag={tag}, scalar_value={scalar_value}, global_step={global_step}')
+                    print(f'add_scalar, log_dir={log_dir}, tag={tag}, scalar_value={scalar_value}, global_step={global_step}')
             case 'add_text':
                 text_string = body.decode()
                 self.get_summary_writer(log_dir).add_text(tag, text_string, global_step)
-                print(f'add_text, tag={tag}, text_string="{text_string}", global_step={global_step}')
+                print(f'add_text, log_dir={log_dir}, tag={tag}, text_string="{text_string}", global_step={global_step}')
             case 'add_figure':
                 with io.BytesIO(body) as b:
                     image_data = pickle.load(b)
                     self.get_summary_writer(log_dir).add_image(tag, image_data, global_step)
-                    print(f'add_figure, tag={tag}, shape(image_data)={image_data.shape}, global_step={global_step}')
+                    print(f'add_figure, log_dir={log_dir},  tag={tag}, shape(image_data)={image_data.shape}, global_step={global_step}')
             case 'add_hparams':
                 with io.BytesIO(body) as b:
                     message = json.load(b)
                     hparam_dict = message['hparam_dict']
                     metric_dict = message['metric_dict']
-                    print(f'add_hparams, hparam_dict={hparam_dict}, metric_dict={metric_dict}, run_name={run_name}')
+                    print(f'add_hparams, log_dir={log_dir}, hparam_dict={hparam_dict}, metric_dict={metric_dict}, run_name={run_name}')
                     self.get_summary_writer(log_dir).add_hparams(hparam_dict, metric_dict, run_name=run_name)
             case 'flush':
                 self.get_summary_writer(log_dir).flush()
                 print('flush')
             case _:
-                assert False, f'Unknown method "{logic_method}"'
+                assert False, f'Unknown method="{logic_method}"'
         
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
@@ -183,7 +184,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--base_log_dir', type=str, default='/logdir')
-    parser.add_argument('--rmq_connection_url', type=str, default='amqp://guest:guest@localhost:5672/%2F')
+    parser.add_argument('--rmq_connection_url', type=str, default='amqp://guest:guest@rabbitmq:5672/%2F')
     args = parser.parse_args()
     print(f'Running collector with args={args}')
     collector = RmqSummaryCollector(args.base_log_dir, args.rmq_connection_url)
