28b74e2796d45e08bd083d2319f55788a4a0fea1
Mikhail Kocherov <ppchaos@mail.ru>
2026-02-25 23:28:06 +0300

    s4_cnn_02.ipynb. Spin of new version of CNN  which is coupled with PSD

---
+++ lib/torch_helpers.py
@@ -0,0 +1,16 @@
+import torch.optim
+
+class LrSchedulerWrapper:
+    def __init__(self, optimizer, hp_learn_rate_params):
+        if hp_learn_rate_params.plateau is None:
+            self.scheduler = None
+            self.step = self.step_dummy
+        else:
+            self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, **hp_learn_rate_params.plateau._asdict())
+            self.step = self.step_plateau
+
+    def step_dummy(self):
+        pass
+    
+    def step_plateau(self, loss):
+        self.scheduler.step(loss)
