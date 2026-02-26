s4_cnn_02.ipynb. 99.49% test accuracy achieved. Also fixed problem with PSD computation - no image normalization was applied. After enabling SampleWiseScaler Gabor filters immediatelly popped up

lib/torch_helpers.py
@@ -9,7 +9,7 @@ class LrSchedulerWrapper:
             self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, **hp_learn_rate_params.plateau._asdict())
             self.step = self.step_plateau
 
-    def step_dummy(self):
+    def step_dummy(self, loss):
         pass
     
     def step_plateau(self, loss):