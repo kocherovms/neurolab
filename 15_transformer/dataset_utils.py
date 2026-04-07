8947028d0ce15950dee51115b4f32b006baa7930
Mikhail Kocherov <ppchaos@mail.ru>
2026-04-07 14:21:49 +0300

    15_transformer. First implementation of predict module

--- 15_transformer/dataset_utils.py
+++ 15_transformer/dataset_utils.py
@@ -6,9 +6,9 @@ import pandas as pd
 
 load_meta = lambda db_con: next(pd.read_sql('SELECT * FROM meta LIMIT 1', con=db_con).itertuples())
 
-load_vocab_tokens = lambda db_con: pd.read_sql('SELECT * FROM vocab_tokens', con=db_con)
+load_vocab_tokens = lambda db_con: pd.read_sql('SELECT * FROM vocab_tokens', con=db_con, index_col='token_ind')
 
-load_pos_tokens = lambda db_con: pd.read_sql('SELECT * FROM pos_tokens', con=db_con)
+load_pos_tokens = lambda db_con: pd.read_sql('SELECT * FROM pos_tokens', con=db_con, index_col='token_ind')
 
 def load_image(image_ind, db_con):
     data = db_con.cursor().execute('SELECT data FROM images WHERE image_ind=:image_ind', dict(image_ind=int(image_ind))).fetchone()[0]
