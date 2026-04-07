805ae7bef523e9c831c8456165f157b24c8ed59f
Mikhail Kocherov <ppchaos@mail.ru>
2026-04-07 14:58:42 +0300

    torch_helpers. Add eval_guard / train_guard

--- lib/torch_helpers.py
+++ lib/torch_helpers.py
@@ -14,3 +14,31 @@ class LrSchedulerWrapper:
     
     def step_plateau(self, loss):
         self.scheduler.step(loss)
+
+class ModelModeContextManager:
+    def __init__(self, model, target_mode):
+        self.training = model.training
+        self.model = model
+
+        match target_mode:
+            case 'eval': self.model.eval()
+            case 'train': self.model.train()
+            case _: assert False, f'Unsupported {target_mode=}'
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.training:
+            self.model.train()
+        else:
+            self.model.eval()
+            
+        return False
+
+def eval_guard(model):
+    return ModelModeContextManager(model, 'eval')
+
+def train_guard(model):
+    return ModelModeContextManager(model, 'train')
+    
