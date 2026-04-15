ee05dd12e84fd79adafafa7dd698338c3dd81ec6
Mikhail Kocherov <ppchaos@mail.ru>
2026-04-15 15:42:37 +0300

    15_transformer. Start work on 15g_explore_combo_noncausal_01

--- 15_transformer/models.py
+++ 15_transformer/models.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from collections import namedtuple
 
 import torch
 import torch.nn as nn
@@ -35,3 +36,41 @@ class NoncausalTransformer(nn.Module):
             x = attach_task_specific_info_fn(x)
             
         return self.encoder(x, is_causal=False)
+
+
+class ComboModel(nn.Module):
+    @dataclass 
+    class Params:
+        vocab_size: int = None
+        pos_tokens_count: int = None
+        embedding_size: int = None # aka d_model
+        heads_count: int = None
+        layers_count: int = None
+        
+    def __init__(self, params):
+        super().__init__()
+        self.params = params
+        self.cls_vocab_embedding = nn.Parameter(torch.zeros(1, 1, params.embedding_size))
+        self.cls_pos_embedding = nn.Parameter(torch.zeros(1, 1, params.embedding_size))
+        ntp = NoncausalTransformer.Params(**dataclasses.asdict(params))
+        self.transformer = NoncausalTransformer(ntp) 
+        self.predictor = nn.Linear(params.embedding_size, params.vocab_size)
+        self.classifier = nn.Linear(params.embedding_size, 10)
+
+    ForwardResult = namedtuple('ForwardResult', 'pred, cls')
+
+    def forward(self, vocab_token_inds, pos_token_inds):
+        def attach_cls(x):
+            batch_size = len(x)
+            cls_tokens = self.cls_vocab_embedding + self.cls_pos_embedding
+            cls_tokens = cls_tokens.expand(batch_size, -1, -1) # cls_tokens shape becomes [batch, 1, emb_size]
+            x = torch.cat((cls_tokens, x), dim=1) # [batch, 1+seq_len, emb_size]
+            return x
+            
+        output = self.transformer(vocab_token_inds, pos_token_inds, attach_cls)
+        cls_output = output[:,0,:] # [batch, emb_size]
+        main_output = output[:,1:,:] # [batch, seq_len, emb_size]
+        return ComboModel.ForwardResult(
+            pred=self.predictor(main_output), 
+            cls=self.classifier(cls_output)
+        )
